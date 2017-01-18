b200de1bd4fe77d535dcab8f19fa0f9a4f649b1c
hachtman <jackfllr1@googlemail.com>
2017-01-18 10:08:38 +0000

    adding process.env

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 //Port
 const port   = process.env.port || 3000;
-const db     = 'mongodb://localhost/where-am-i';
+const db     = process.env.MONGODB_URI || 'mongodb://localhost/where-am-i';
 const secret = process.env.SECRET || 'hiding in plain sight';
 //Exports
 module.exports = {
